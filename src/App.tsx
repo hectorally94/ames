a0610aaffa7bf5db1b06652f59bf6b0ebdf5d17e
Ramadhani Ally <hectorally94@gmail.com>
2024-06-29 15:43:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="text-4xl text-blue-500">
+        
+        let start building ames projet
+      </p>
+    </>
+  )
+}
+
+export default App
