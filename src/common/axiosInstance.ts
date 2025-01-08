3c4d83a9ff59fe9b5b6baecf49a614a69cd60ac2
Ramadhani Ally <hectorally94@gmail.com>
2025-01-08 10:26:29 +0200

    init

--- src/common/axiosInstance.ts
+++ src/common/axiosInstance.ts
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'https://amues.org/api',  // Use the secure HTTPS endpoint
-  baseURL: 'http://localhost:8088/api',
-// baseURL: 'http://172.105.33.252:8088/api',
+  baseURL: 'https://amues.org/api',  // Use the secure HTTPS endpoint
+//  baseURL: 'http://localhost:8088/api',
+ // baseURL: 'http://172.105.33.252:8088/api',
 
   headers: {
     'Content-type': 'application/json',
