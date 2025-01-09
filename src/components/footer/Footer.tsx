18eb35fe81e083e3d2b1504edbffbf652b6bcf70
Ramadhani Ally <hectorally94@gmail.com>
2025-01-09 11:59:19 +0200

    init

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-700 dark:border-gray-600">
       <span className=" font-slab text-sm text-gray-500 sm:text-center dark:text-gray-100">
-        © {currentYear} {t('developedBy')} Ir Ally
+        © {currentYear} {t('developed By')} AMUES
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-100 sm:mt-0">
         <li className="mr-4">
